3d4d22570fd1810a5d316238235353e322207434
jhkerschner <jhkerschner@gmail.com>
2016-12-27 15:10:39 -0500

    Set up social media links on the site settings page, made them dynamic in the footer.
    Changed the name of the linked SVG so it will work with the dynamic
    naming.

--- build/wp-content/themes/landmark/footer.php
+++ build/wp-content/themes/landmark/footer.php
@@ -4,18 +4,17 @@
     <footer class="site-footer">
       <div class="container">
         <div class="footer-left-col">
-          <a class="footer-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php include (get_template_directory().'/assets/images/footer-logo.svg'); ?></a>
+          <a class="footer-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home" data-event-category="In-Page Nav" data-event-action="Nav Click" data-event-label="Footer Home Logo"><?php include (get_template_directory().'/assets/images/footer-logo.svg'); ?></a>
         </div>
         <div class="footer-right-col">
+          <?php if(have_rows('social_media_links', 'option')) : ?>
           <div class="social-icons">
-            <a href="#" class="social-icon icon-facebook"><?php include (get_template_directory().'/assets/images/facebook.svg'); ?></a>
-            <a href="#" class="social-icon icon-twitter"><?php include (get_template_directory().'/assets/images/twitter.svg'); ?></a>
-            <a href="#" class="social-icon icon-linkedin"><?php include (get_template_directory().'/assets/images/linkedin2.svg'); ?></a>
-            <a href="#" class="social-icon icon-foursquare"><?php include (get_template_directory().'/assets/images/foursquare.svg'); ?></a>
-            <a href="#" class="social-icon icon-yelp"><?php include (get_template_directory().'/assets/images/yelp.svg'); ?></a>
-            <a href="#" class="social-icon icon-instagram"><?php include (get_template_directory().'/assets/images/instagram.svg'); ?></a>
-            <a href="#" class="social-icon icon-youtube"><?php include (get_template_directory().'/assets/images/youtube.svg'); ?></a>
+          <?php while(have_rows('social_media_links', 'option')) : the_row(); 
+                $platform = get_sub_field('social_media_platform');?>
+                  <a href="<?php the_sub_field('url'); ?>" target="_blank" class="social-icon icon-<?php echo $platform['value']; ?>" data-event-category="External Link" data-event-action="Click" data-event-label="<?php echo $platform['label'] ?>"><?php include (get_template_directory().'/assets/images/'.$platform['value'].'.svg'); ?></a>
+          <?php endwhile; ?>
           </div>
+          <?php endif; ?>
           <p class="copyright">&copy; Landmark Theatre 362 S. Salina Street, Syracuse, NY 13202</p>
         </div>
       </div>  
