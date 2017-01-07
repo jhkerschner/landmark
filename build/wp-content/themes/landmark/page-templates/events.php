b5c5dce2b6145d7bd320de27809c93d19258dff4
John Woolschlager <wullaski@gmail.com>
2017-01-07 12:10:59 -0500

    styling events and membership

--- build/wp-content/themes/landmark/page-templates/events.php
+++ build/wp-content/themes/landmark/page-templates/events.php
@@ -8,23 +8,23 @@ get_header();
 ?>
 <div class="container">
 	<div class="row">
-		<div class="left-col">
+		<div class="left-col events">
+			<h1>Events</h1>
 			<?php 
 			$events = get_landmark_events();
 			foreach($events as $event) :
 				$event_date = process_event_dates($event['dates']);
 			?>
 				<div class="event">
-					<img src="<?php echo $event['image']; ?>" style="float:left:">
-					<div style="float:left;">
-						<p class="event-title"><?php echo $event['title']; ?></p>
+					<img class="event-image" src="<?php echo $event['image']; ?>">
+					<div class="event-info">
+						<h2 class="event-title"><?php echo $event['title']; ?></h2>
 						<p class="event-date"><?php echo $event_date; ?></p>
 						<?php echo $event['description']; //already has <p> tags ?>
 						<?php if($event['ticket_link'] != '') : ?>
-							<a href="<?php echo $event['ticket_link']; ?>" target="_blank">Tickets</a>
+							<a class="cta-button" href="<?php echo $event['ticket_link']; ?>" target="_blank">Tickets</a>
 						<?php endif; ?>
 					</div>
-					<div style="clear:both;"></div>
 				</div>
 			<?php endforeach; ?>
 		</div>
