4a985f9728e379b2a911b72b6e4b89832bc26694
jhkerschner <jhkerschner@gmail.com>
2017-01-22 16:27:30 -0500

    Removed woocommerce meta boxes that are not needed. Removed the category meta data on a product page.

--- build/wp-content/themes/landmark/lib/woocommerce.php
+++ build/wp-content/themes/landmark/lib/woocommerce.php
@@ -12,3 +12,41 @@ remove_action( 'woocommerce_shop_loop_item_title', 'woocommerce_template_loop_pr
 
 /* ADD new loop-title-with sku action      */
 add_action( 'woocommerce_shop_loop_item_title', 'landmark_woocommerce_template_loop_product_title', 10 );
+
+/** Remove short description if product tabs are not displayed */
+function dot_reorder_product_page() {
+    if ( get_option('woocommerce_product_tabs') == false ) {
+        remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
+    }
+}
+add_action( 'woocommerce_before_main_content', 'dot_reorder_product_page' );
+
+/** Display product description the_content */
+function dot_do_product_desc() {
+
+    global $woocommerce, $post;
+
+    if ( $post->post_content ) : ?>
+        <div itemprop="description" class="item-description">
+            <?php $heading = apply_filters('woocommerce_product_description_heading', __('Product Description', 'woocommerce')); ?>
+
+            <!-- <h2><?php echo $heading; ?></h2> -->
+            <?php the_content(); ?>
+
+        </div>
+    <?php endif;
+}
+add_action( 'woocommerce_single_product_summary', 'dot_do_product_desc', 20 );
+
+
+/**
+ * Remove product short description meta box
+ */
+add_action( 'add_meta_boxes' , 'remove_wc_metaboxes', 50 );
+function remove_wc_metaboxes() {
+     remove_meta_box( 'postcustom' , 'product' , 'normal' );
+     remove_meta_box( 'postexcerpt' , 'product' , 'normal' );
+     remove_meta_box( 'commentsdiv' , 'product' , 'normal' );
+     remove_meta_box( 'tagsdiv-product_tag' , 'product' , 'side' );
+     remove_meta_box( 'woocommerce-product-images' , 'product' , 'side' );
+}
