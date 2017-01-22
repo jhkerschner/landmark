d34c3a7c424da655f33c56bb8fa7061900029524
jhkerschner <jhkerschner@gmail.com>
2017-01-21 19:44:10 -0500

    WooCommerece stuff

--- build/wp-content/themes/landmark/woocommerce/archive-product.php
+++ build/wp-content/themes/landmark/woocommerce/archive-product.php
@@ -45,7 +45,7 @@ get_header( 'shop' ); ?>
 			 * @hooked woocommerce_taxonomy_archive_description - 10
 			 * @hooked woocommerce_product_archive_description - 10
 			 */
-			do_action( 'woocommerce_archive_description' );
+			//do_action( 'woocommerce_archive_description' );
 		?>
 
 		<?php if ( have_posts() ) : ?>
