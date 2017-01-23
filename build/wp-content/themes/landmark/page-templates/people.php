c81b2a57be66830a08e7b5cbb4cb7e2b624755c0
jhkerschner <jhkerschner@gmail.com>
2017-01-23 18:41:25 -0500

    Added more sections to the page.

--- build/wp-content/themes/landmark/page-templates/people.php
+++ build/wp-content/themes/landmark/page-templates/people.php
@@ -8,7 +8,8 @@ get_header(); ?>
     <div class="left-col">
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <h1 class="entry-title"><?php the_title(); ?></h1>
-        <div class="wysiwyg"><?php the_content(); ?></div>
+
+        <div class="wysiwyg"><?php the_field('team_copy'); ?></div>
         <?php
         if(have_rows('team')) : 
           while(have_rows('team')) : the_row(); ?>
@@ -20,6 +21,25 @@ get_header(); ?>
               </div>
             </div>
         <?php endwhile; endif; ?>
+
+        <div class="wysiwyg"><?php the_field('volunteer_copy'); ?></div>
+
+        <div class="wysiwyg"><?php the_field('board_of_directors_copy'); ?></div>
+
+        <?php
+        if(have_rows('board_of_directors')) : 
+          while(have_rows('board_of_directors')) : the_row(); ?>
+            <div class="person">
+              <p class="name"><?php the_sub_field('name'); ?></p>
+              <p class="title"><?php the_sub_field('title_on_board'); ?></p>
+              <p class="title"><?php the_sub_field('titlecurrent_company'); ?></p>
+              <div class="wysiwyg">
+                <?php the_sub_field('short_description'); ?>
+              </div>
+            </div>
+        <?php endwhile; endif; ?>
+
+
       <?php endwhile; endif; ?>
     </div>
     <div class="right-col">
