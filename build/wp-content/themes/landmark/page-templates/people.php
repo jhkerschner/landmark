d2119791a389b842208ad5953f50179165fa2648
John Woolschlager <wullaski@gmail.com>
2017-01-23 23:06:34 -0500

    styled people section

--- build/wp-content/themes/landmark/page-templates/people.php
+++ build/wp-content/themes/landmark/page-templates/people.php
@@ -10,34 +10,37 @@ get_header(); ?>
         <h1 class="entry-title"><?php the_title(); ?></h1>
 
         <div class="wysiwyg"><?php the_field('team_copy'); ?></div>
-        <?php
-        if(have_rows('team')) : 
-          while(have_rows('team')) : the_row(); ?>
-            <div class="person">
-              <p class="name"><?php the_sub_field('name'); ?></p>
-              <p class="title"><?php the_sub_field('title'); ?></p>
-              <div class="wysiwyg">
-                <?php the_sub_field('bio'); ?>
+        <div class="people">
+          <?php
+          if(have_rows('team')) : 
+            while(have_rows('team')) : the_row(); ?>
+              <div class="person">
+                <h2 class="name"><?php the_sub_field('name'); ?></h2>
+                <p class="title"><?php the_sub_field('title'); ?></p>
+                <div class="wysiwyg">
+                  <?php the_sub_field('bio'); ?>
+                </div>
               </div>
-            </div>
-        <?php endwhile; endif; ?>
+          <?php endwhile; endif; ?>
+        </div>
 
         <div class="wysiwyg"><?php the_field('volunteer_copy'); ?></div>
 
         <div class="wysiwyg"><?php the_field('board_of_directors_copy'); ?></div>
-
-        <?php
-        if(have_rows('board_of_directors')) : 
-          while(have_rows('board_of_directors')) : the_row(); ?>
-            <div class="person">
-              <p class="name"><?php the_sub_field('name'); ?></p>
-              <p class="title"><?php the_sub_field('title_on_board'); ?></p>
-              <p class="title"><?php the_sub_field('titlecurrent_company'); ?></p>
-              <div class="wysiwyg">
-                <?php the_sub_field('short_description'); ?>
+        <div class="people">
+          <?php
+          if(have_rows('board_of_directors')) : 
+            while(have_rows('board_of_directors')) : the_row(); ?>
+              <div class="person">
+                <h2 class="name"><?php the_sub_field('name'); ?></h2>
+                <p class="title"><?php the_sub_field('title_on_board'); ?></p>
+                <p class="title"><?php the_sub_field('titlecurrent_company'); ?></p>
+                <div class="wysiwyg">
+                  <?php the_sub_field('short_description'); ?>
+                </div>
               </div>
-            </div>
-        <?php endwhile; endif; ?>
+          <?php endwhile; endif; ?>
+        </div>
 
 
       <?php endwhile; endif; ?>
