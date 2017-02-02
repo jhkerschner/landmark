072175563d7852132e1625cbeeafae3f11c73b64
jhkerschner <jhkerschner@gmail.com>
2017-02-01 20:25:51 -0500

    Product image changes

--- build/wp-content/themes/landmark/lib/woocommerce.php
+++ build/wp-content/themes/landmark/lib/woocommerce.php
@@ -3,8 +3,16 @@
 remove_action( 'woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 10 );
 add_action( 'woocommerce_shop_loop_item_title', 'landmark_woocommerce_template_loop_product_title', 10 );
 function landmark_woocommerce_template_loop_product_title() {
-	echo '<div><h3 class="loop-title">' . get_the_title() . '</h3></div>';
+	echo '<div class="title-price"><h3 class="loop-title">' . get_the_title() . '</h3></div>';
 }
+
+/* WC archive image */
+remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10 );
+add_action( 'woocommerce_before_shop_loop_item_title', 'landmark_woocommerce_template_loop_product_image', 10 );
+function landmark_woocommerce_template_loop_product_image() {
+    echo woocommerce_get_product_thumbnail('full');
+}
+
 
 add_action( 'woocommerce_before_main_content', 'dot_reorder_product_page' );
 
